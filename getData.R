821ff92d1dee440c2b8b429cd269989879ddc66f
Adam Weimerskirch <adamweimerskirch@gmail.com>
2019-07-28 14:27:49 -0500

    remove unneccesary variables, create variable to choose boulder / route difficulty scale

--- getData.R
+++ getData.R
@@ -11,7 +11,7 @@ ascentData <- dbGetQuery(
   con,
   "SELECT a.user_id, a.grade_id, a.method_id, a.climb_type, a.total_score, a.year, a.name, a.crag_id, a.crag, a.sector_id, a.sector, a.country, a.comment, a.rating, a.user_recommended, a.chipped,
   m.score as method_score, m.shorthand,
-  g.score as grade_score, g.usa_routes, g.usa_routes_input, g.usa_routes_selector, g.usa_boulders, g.usa_boulders_input, g.usa_boulders_selector,
+  g.score as grade_score, g.usa_routes, g.usa_boulders,
   u.city as user_city, u.country as user_country, u.sex, u.height, u.weight, u.started, u.occupation, u.best_area, u.worst_area, u.interests, u.birth
   FROM ascent a
   LEFT JOIN method m ON a.method_id = m.id
@@ -19,13 +19,16 @@ ascentData <- dbGetQuery(
   LEFT JOIN user u ON a.user_id = u.id"
 )
 
+# add contextual difficulty variable
+ascentData <- ascentData %>% transform(difficulty = ifelse(climb_type == 1, usa_boulders, usa_routes)) %>% within(rm(usa_routes, usa_boulders))
+
 # convert appropriate variables to title case
 ascentData$name <- stri_trans_totitle(ascentData$name)
 ascentData$crag <- stri_trans_totitle(ascentData$crag)
 ascentData$sector <- stri_trans_totitle(ascentData$sector)
 ascentData$country <- stri_trans_toupper(ascentData$country)
 ascentData$user_city <- stri_trans_totitle(ascentData$user_city)
-ascentData$user_country <- stri_trans_totitle(ascentData$user_country)
+ascentData$user_country <- stri_trans_toupper(ascentData$user_country)
 
 # filter to random set of ascents
 miniSample <- sample(1:nrow(ascentData), 100)
